9bc07a35b3d93148feda629b2e3e03805cb2bd5f
Aditya Burugu <adityaburugu1234567@gmail.com>
2020-09-19 16:35:49 +0530

    Open to See

--- Odd_Even/Odd_Even.py
+++ Odd_Even/Odd_Even.py
@@ -1,24 +1,32 @@
 #Sum and Average of Odd Numbers and Sum and Average of Even Numbers.
-L=int(input('Enter the Minimum Range: '))
-U=int(input('Enter the Maximum Range: '))
-Even_sum=0
-Even_numbers=0
-Odd_sum=0
-Odd_numbers=0
-for i in range(L,U+1):
-    if(i%2==0):
-        Even_sum+=i
-        Even_numbers+=1
-    elif(i%2!=0):
-        Odd_sum+=i
-        Odd_numbers+=1
-print(Even_sum,'is the sum of Even Numbers from',L,'to',U)
-print(Odd_sum,'is the sum of Odd numbers from',L,'to',U)
-print('There are',Even_numbers,'between',L,'and',U)
-print('There are',Odd_numbers,'Between',L,'and',U)
-Average_Even=Even_sum/Even_numbers
-Average_Odd=Odd_sum/Odd_numbers
-print(Average_Even,'is the Average of Even Numbers from',L,'to',U)
-print(Average_Odd,'is the Average of Odd numbers from',L,'to',U)
-print('Hi Aditya Your Program execution is Completed')
-print('Thank You')
+def Odd_Even(LowerLimit,UpperLimit):
+    L = LowerLimit
+    U = UpperLimit
+    Even_Numbers = []
+    Odd_Numbers = []
+    Even_sum = 0
+    Even_numbers = 0
+    Odd_sum = 0
+    Odd_numbers = 0
+    for i in range(L, U + 1):
+        if (i % 2 == 0):
+            Even_Numbers.append(i)
+            Even_sum += i
+            Even_numbers += 1
+        elif (i % 2 != 0):
+            Odd_Numbers.append(i)
+            Odd_sum += i
+            Odd_numbers += 1
+    print(Even_sum, 'is the sum of Even Numbers from', L, 'to', U)
+    print(Odd_sum, 'is the sum of Odd numbers from', L, 'to', U)
+    print('There are', Even_numbers, 'Even Numbers from', L, 'to', U)
+    print("Even Numbers from", L, 'to', U, 'are', Even_Numbers)
+    print('There are', Odd_numbers, 'Odd Numbers from', L, 'to', U)
+    print("Odd Numbers from", L, 'to', U, 'are', Odd_Numbers)
+    Average_Even = Even_sum / Even_numbers
+    Average_Odd = Odd_sum / Odd_numbers
+    print(Average_Even, 'is the Average of Even Numbers from', L, 'to', U)
+    print(Average_Odd, 'is the Average of Odd numbers from', L, 'to', U)
+    print('Hi Aditya Your Program execution is Completed')
+    print('Thank You')
+Odd_Even(3,8)
--- Prime/Prime.py
+++ Prime/Prime.py
@@ -1,15 +1,26 @@
-print('Finding Prime Numbers and Sum of Prime Numbers in a given Range')
-L = int(input('Enter the Minimum Range: '))
-U = int(input('Enter the Maximum Range: '))
-Prime = []
-for N in range(L,U+1):
-    Factors = 0
-    for Divisor in range(1,N+1):
-        if (N % Divisor == 0):
-            Factors+=1
-    if(Factors == 2):
-        print(N,'is a prime number')
-        Prime.append(N)
-print('The sum of Prime Numbers Between {:d} to {:d} is {:d}'.format(L,U,sum(Prime)))
-print('Program Finished')
-print('Thank You')
+def Prime(LowerLimit,UpperLimit):
+    L = LowerLimit
+    U = UpperLimit
+    Prime = []
+    if LowerLimit<UpperLimit and LowerLimit>0 and UpperLimit>0:
+        print('Finding Prime Numbers and Sum of Prime Numbers in a given Range')
+        for N in range(L, U + 1):
+            Factors = 0
+            for Divisor in range(1, N + 1):
+                if (N % Divisor == 0):
+                    Factors += 1
+            if (Factors == 2):
+                print(N, 'is a prime number')
+                Prime.append(N)
+        print('The sum of Prime Numbers Between {:d} to {:d} is {:d}'.format(L, U, sum(Prime)))
+        print('Program Finished')
+        print('Thank You')
+    elif LowerLimit>=UpperLimit:
+        print("Lower Limit Should be less than Upper Limit")
+    elif LowerLimit<0 and UpperLimit<0:
+        print("Invalid Lower & Upper Limit Input")
+    elif LowerLimit<0:
+        print("Invalid Lower Limit Input")
+    elif UpperLimit<0:
+        print("Invalid Upper Limit Input")
+Prime(10,100)
